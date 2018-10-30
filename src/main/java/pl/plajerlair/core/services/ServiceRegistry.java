c85390a0ed5d29efdbd9cd0f3ea8a3098a193cda
Plajer <worthplay.pt@onet.pl>
2018-10-30 16:29:37 +0100

    Service update

--- src/main/java/pl/plajerlair/core/services/ServiceRegistry.java
+++ src/main/java/pl/plajerlair/core/services/ServiceRegistry.java
@@ -4,13 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import pl.plajerlair.core.services.locale.LocaleService;
 import pl.plajerlair.core.services.metrics.MetricsService;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.net.URL;
 import java.util.logging.Level;
 
 /**
@@ -24,18 +20,24 @@ public class ServiceRegistry {
     private static LocaleService localeService;
 
     public static boolean registerService(JavaPlugin plugin) {
-        if(registeredService.equals(plugin)) {
+        if(registeredService != null && registeredService.equals(plugin)) {
             return false;
         }
+        plugin.getLogger().log(Level.INFO, "Connecting to services, please wait! Server may freeze a bit!");
         try {
-            plugin.getLogger().log(Level.INFO, "Connecting to services, please wait! Server may freeze a bit!");
-            if(!InetAddress.getByName("https://api.plajer.xyz/locale/fetch.php").isReachable(3 * 1000)) {
-                plugin.getLogger().log(Level.WARNING, "Plajer's Lair services are online or inaccessible from your location!");
+            HttpsURLConnection connection = (HttpsURLConnection) new URL("https://plajer.xyz").openConnection();
+            connection.setConnectTimeout(3000);
+            connection.setReadTimeout(2000);
+            connection.setRequestMethod("HEAD");
+            connection.setRequestProperty("User-Agent", "PLService/1.0");
+            int responseCode = connection.getResponseCode();
+            if(responseCode != 200) {
+                plugin.getLogger().log(Level.WARNING, "Plajer's Lair services aren't online or inaccessible from your location! Response: " + responseCode + ". Do you think it's site problem? Contact developer! Make sure Cloudflare isn't blocked in your area!");
                 serviceEnabled = false;
                 return false;
             }
         } catch(IOException ignored) {
-            plugin.getLogger().log(Level.WARNING, "Plajer's Lair services are online or inaccessible from your location!");
+            plugin.getLogger().log(Level.WARNING, "Plajer's Lair services aren't online or inaccessible from your location!");
             serviceEnabled = false;
             return false;
         }
