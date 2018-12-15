8507230844def2a74b0113a550121bbcb30d27a2
Plajer <worthplay.pt@onet.pl>
2018-12-15 14:45:19 +0100

    Implemented Debugger

---
+++ src/main/java/pl/plajerlair/core/debug/Debugger.java
@@ -0,0 +1,57 @@
+package pl.plajerlair.core.debug;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+
+/**
+ * @author Plajer
+ * <p>
+ * Created at 15.12.2018
+ *
+ * Class for printing debug messages
+ */
+public class Debugger {
+
+    private static boolean enabled = false;
+    private static String prefix = "";
+
+    public void setPrefix(String prefix) {
+        Debugger.prefix = prefix;
+    }
+
+    public void setEnabled(boolean enabled) {
+        Debugger.enabled = enabled;
+    }
+
+    /**
+     * Prints debug message with selected log level
+     * @param level level of debugged message
+     * @param thing debugged message
+     */
+    public static void debug(LogLevel level, String thing) {
+        if (enabled) {
+            switch (level) {
+                case INFO:
+                    Bukkit.getConsoleSender().sendMessage(prefix + " " + thing);
+                    break;
+                case WARN:
+                    Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + prefix + " " + thing);
+                    break;
+                case ERROR:
+                    Bukkit.getConsoleSender().sendMessage(ChatColor.RED + prefix + " " + thing);
+                    break;
+                case WTF:
+                    Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + prefix + " [SEVERE]" + thing);
+                    break;
+                case TASK:
+                    Bukkit.getConsoleSender().sendMessage(ChatColor.YELLOW + prefix + " Running task '" + thing + "'");
+                    break;
+            }
+        }
+    }
+
+    public enum LogLevel {
+        INFO, WARN, ERROR, WTF /* what a terrible failure */, TASK,
+    }
+
+}
