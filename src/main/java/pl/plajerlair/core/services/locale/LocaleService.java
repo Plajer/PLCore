a2b8310490e4a1eda94c933ae17dd1395e91623b
Plajer <worthplay.pt@onet.pl>
2018-12-17 14:29:28 +0100

    Locale Service updated

--- src/main/java/pl/plajerlair/core/services/locale/LocaleService.java
+++ src/main/java/pl/plajerlair/core/services/locale/LocaleService.java
@@ -10,6 +10,7 @@ import pl.plajerlair.core.utils.internal.InternalUtils;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -35,20 +36,16 @@ public class LocaleService {
         this.plugin = plugin;
         try {
             String data = IOUtils.toString(requestLocaleFetch(null), StandardCharsets.UTF_8);
-            FileUtils.write(new File(plugin.getDataFolder().getPath() + "/locales/data.yml"), data, StandardCharsets.UTF_8);
-            if(!new File(plugin.getDataFolder().getPath() + "/locales/origin_data.yml").exists()) {
-                FileUtils.write(new File(plugin.getDataFolder().getPath() + "/locales/origin_data.yml"), data, StandardCharsets.UTF_8);
-            }
-            this.localeData = ConfigUtils.getConfig(plugin, "/locales/data");
-            //-1 cause not including "valid-version" param
-            plugin.getLogger().log(Level.INFO, "Fetched latest localizations, " + (localeData.getConfigurationSection("locales").getKeys(false).size() - 1) + " locales were fetched.");
+            FileUtils.write(new File(plugin.getDataFolder().getPath() + "/locales/locale_data.yml"), data, StandardCharsets.UTF_8);
+            this.localeData = ConfigUtils.getConfig(plugin, "/locales/locale_data");
+            plugin.getLogger().log(Level.INFO, "Fetched latest localization file from repository.");
         } catch(IOException ignored) {
             //ignore exceptions
             plugin.getLogger().log(Level.WARNING, "Couldn't access locale fetcher service or there is other problem! You should notify author!");
         }
     }
 
-    private InputStream requestLocaleFetch(String locale) {
+    private InputStream requestLocaleFetch(Locale locale) {
         try {
             //todo /v2/
             URL url = new URL("https://api.plajer.xyz/locale/fetch.php");
@@ -62,7 +59,7 @@ public class LocaleService {
             if(locale == null) {
                 os.write(("pass=localeservice&type=" + plugin.getName()).getBytes(StandardCharsets.UTF_8));
             } else {
-                os.write(("pass=localeservice&type=" + plugin.getName() + "&locale=" + locale).getBytes(StandardCharsets.UTF_8));
+                os.write(("pass=localeservice&type=" + plugin.getName() + "&locale=" + locale.getPrefix()).getBytes(StandardCharsets.UTF_8));
             }
             os.flush();
             os.close();
@@ -83,35 +80,28 @@ public class LocaleService {
      * Sends a demand request to download latest locale from Plajer-Lair/locale_storage repository
      * Whole repository can be seen here https://github.com/Plajer-Lair/locale_storage
      *
-     * @param locale locale prefix to download
+     * @param locale locale to download
      * @return SUCCESS for downloaded locale, FAIL for service fault, LATEST when locale is latest as one in repository
      */
-    public DownloadStatus demandLocaleDownload(String locale) {
+    public DownloadStatus demandLocaleDownload(Locale locale) {
         //service fault
         if(localeData == null) {
             return DownloadStatus.FAIL;
         }
-        if(!new File(plugin.getDataFolder() + "/locales/" + locale + ".properties").exists()) {
+        File localeFile = new File(plugin.getDataFolder() + "/locales/" + locale.getPrefix() + ".properties");
+        if(!(localeFile.exists() || isExact(locale, localeFile))) {
             return writeFile(locale);
         }
-        if(localeData.getInt("locales." + locale + ".version") > ConfigUtils.getConfig(plugin, "/locales/origin_data").getInt("locales." + locale + ".version", -1)) {
-            return writeFile(locale);
-        } else {
-            return DownloadStatus.LATEST;
-        }
+        return DownloadStatus.LATEST;
     }
 
-    private DownloadStatus writeFile(String locale) {
+    private DownloadStatus writeFile(Locale locale) {
         try {
             String data = IOUtils.toString(requestLocaleFetch(locale), StandardCharsets.UTF_8);
-            FileUtils.write(new File(plugin.getDataFolder().getPath() + "/locales/" + locale + ".properties"), data, StandardCharsets.UTF_8);
-
-            FileConfiguration config = ConfigUtils.getConfig(plugin, "/locales/origin_data");
-            config.set("locales." + locale, localeData.getInt("locales." + locale, 0));
-            ConfigUtils.saveConfig(plugin, config, "/locales/origin_data");
+            FileUtils.write(new File(plugin.getDataFolder().getPath() + "/locales/" + locale.getPrefix() + ".properties"), data, StandardCharsets.UTF_8);
             return DownloadStatus.SUCCESS;
         } catch(IOException ignored) {
-            plugin.getLogger().log(Level.WARNING, "Demanded locale " + locale + " cannot be downloaded! You should notify author!");
+            plugin.getLogger().log(Level.WARNING, "Demanded locale " + locale.getPrefix() + " cannot be downloaded! You should notify author!");
             return DownloadStatus.FAIL;
         }
     }
@@ -126,27 +116,24 @@ public class LocaleService {
         if(localeData == null) {
             return false;
         }
-        return !checkHigher(plugin.getDescription().getVersion(), localeData.getString("locales.valid-version", "0"));
+        return !checkHigher(plugin.getDescription().getVersion(), localeData.getString("locales.valid-version", plugin.getDescription().getVersion()));
+    }
+
+    private boolean isExact(Locale locale, File file) {
+        try {
+            String onlineData = IOUtils.toString(requestLocaleFetch(locale), StandardCharsets.UTF_8);
+            String localData = IOUtils.toString(new FileInputStream(file), StandardCharsets.UTF_8);
+
+            return onlineData.equals(localData);
+        } catch(IOException ignored) {
+            return false;
+        }
     }
 
     private boolean checkHigher(String currentVersion, String newVersion) {
         String current = InternalUtils.toReadable(currentVersion);
         String newVer = InternalUtils.toReadable(newVersion);
         return current.compareTo(newVer) < 0;
-    }
-
-    /**
-     * Returns requested locale version number
-     *
-     * @param locale locale prefix
-     * @return locale version
-     */
-    public int getLocaleVersion(String locale) {
-        //service fault
-        if(localeData == null) {
-            return 0;
-        }
-        return localeData.getInt("locales." + locale + ".version", 69);
     }
 
     /**
